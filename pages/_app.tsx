4ba3f186be17f06ba50c124bb9a6d75b20690a87
ella <ella@choeja-eun-ui-MacBookAir.local>
2023-03-23 02:56:49 +0900

    충돌 해결

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Layout from "@/src/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
@@ -9,11 +8,4 @@ export default function App({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
     </Layout>
   );
-=======
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
->>>>>>> 5cb87988c8477ef3040e85d008b2d897e8f5cb43
 }
