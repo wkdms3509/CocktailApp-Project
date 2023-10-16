a343e8b9f6f90af53a50e451bb02ba734485abd3
ella <ella@choeja-eun-ui-MacBookAir.local>
2023-10-16 16:49:11 +0900

    docker 에러

--- cocktail-app/pages/index.tsx
+++ cocktail-app/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "@/styles/Home.module.css";
+// import styles from "@/styles/Home.module.css";
 import MainBanner from "@/src/components/MainBanner";
 import Catecory from "@/src/components/Catecory";
 import AllCocktailList from "@/src/components/AllCocktailList";
--- cocktail-app/src/reducer/index.ts
+++ cocktail-app/src/reducer/index.ts
@@ -22,11 +22,30 @@ import {
   REHYDRATE,
 } from "redux-persist";
 // import storage from "redux-persist/lib/storage/session";
-import storageSession from "redux-persist/lib/storage/session";
+// import storageSession from "redux-persist/lib/storage/session";
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+const createNoopStorage = () => {
+  return {
+    getItem(_key: any) {
+      return Promise.resolve(null);
+    },
+    setItem(_key: any, value: any) {
+      return Promise.resolve(value);
+    },
+    removeItem(_key: any) {
+      return Promise.resolve();
+    },
+  };
+};
+const storage =
+  typeof window !== "undefined"
+    ? createWebStorage("session")
+    : createNoopStorage();
 
 const persistConfig = {
   key: "root",
-  storage: storageSession,
+  storage,
+  // storage: storageSession,
   whitelist: ["userReducer"],
 };
 
