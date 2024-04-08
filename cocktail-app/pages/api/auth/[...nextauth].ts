7b83d40f13dc980ba4f935da43c65ca24e6c8873
ella <ella@choeja-eun-ui-MacBookAir.local>
2024-04-08 21:02:53 +0900

    에러 throw 수정

--- cocktail-app/pages/api/auth/[...nextauth].ts
+++ cocktail-app/pages/api/auth/[...nextauth].ts
@@ -165,7 +165,7 @@ export const authOptions: NextAuthOptions = {
             idx: user.idx ?? null,
           } as User;
         } catch (error) {
-          throw new Error(error.response.data.msg);
+          throw new Error(error.response.data);
         }
         return null;
       },
